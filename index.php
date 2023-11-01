059a9889273e82faa3a084f85afe6d4a0e1180ef
rmens <r.mens@me.com>
2023-11-01 16:50:46 +0100

    All in one

--- index.php
+++ index.php
@@ -2,26 +2,31 @@
 
 $secret_key = 'YOUR_SECRET_KEY';
 $db_path = '/path/to/your/tasks.db';
-file
 
 if (!isset($_GET['key']) || $_GET['key'] !== $secret_key) {
     die('Unauthorized request.');
 }
 
 $source = $_GET['source'];
+$id = basename(dirname($source));  // Extract ID from the URL
 
 $db = new SQLite3($db_path);
 
 // Create table if it doesn't exist
-$db->exec("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, url 
-TEXT, status TEXT)");
+$db->exec("CREATE TABLE IF NOT EXISTS tasks (id TEXT PRIMARY KEY, url TEXT, status TEXT)");
+
+// Check if the ID is already present in the database
+$result = $db->querySingle("SELECT EXISTS(SELECT 1 FROM tasks WHERE id='$id')");
+
+if ($result) {
+    die("Error: ID already exists in the database.");
+}
 
 // Insert new task with 'queued' status
-$stmt = $db->prepare("INSERT INTO tasks (url, status) VALUES (:url, 
-'queued')");
+$stmt = $db->prepare("INSERT INTO tasks (id, url, status) VALUES (:id, :url, 'queued')");
+$stmt->bindValue(':id', $id);
 $stmt->bindValue(':url', $source);
 $stmt->execute();
 
 echo "Task added.";
 ?>
-
--- worker.sh
+++ worker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DB_PATH="/path/to/your/tasks.db"
+DB_PATH="/path/to/your/tasks.db"  
 
 while true; do
     # Fetch the next 'queued' task from SQLite
@@ -16,33 +16,42 @@ while true; do
     IFS="|" read -r id url <<< "$task"
 
     # Update the task status to 'downloading'
-    sqlite3 $DB_PATH "UPDATE tasks SET status='downloading' WHERE id=$id"
+    sqlite3 $DB_PATH "UPDATE tasks SET status='downloading' WHERE id='$id'"
 
-    # Download the file
-    if ./download.sh "$url"; then
-        # Continue with the rest of the process if download was successful
-        mp4_path="/mnt/media/$(basename $(dirname $url)).mp4"
-        
-        # Convert the file
-        sqlite3 $DB_PATH "UPDATE tasks SET status='converting' WHERE id=$id"
-        if ./convert.sh "$mp4_path"; then
-            # Transcribe the file if conversion was successful
-            wav_path="${mp4_path%.mp4}.wav"
-            sqlite3 $DB_PATH "UPDATE tasks SET status='transcribing' WHERE id=$id"
-            if ./transcribe.sh "$wav_path"; then
-                # Mark task as 'completed'
-                sqlite3 $DB_PATH "UPDATE tasks SET status='completed' WHERE id=$id"
-            else
-                # If any script fails, update the task status to 'failed'
-                sqlite3 $DB_PATH "UPDATE tasks SET status='failed' WHERE id=$id"
-            fi
-        else
-            sqlite3 $DB_PATH "UPDATE tasks SET status='failed' WHERE id=$id"
-        fi
-    else
-        sqlite3 $DB_PATH "UPDATE tasks SET status='failed' WHERE id=$id"
+    # 1. Download the file
+    OUTPUT_PATH="/mnt/media/$id.mp4"
+    if ! curl -o $OUTPUT_PATH $url; then
+        sqlite3 $DB_PATH "UPDATE tasks SET status='download_failed' WHERE id='$id'"
+        continue
     fi
 
-    sleep 1
-done
+    # Update the task status to 'converting'
+    sqlite3 $DB_PATH "UPDATE tasks SET status='converting' WHERE id='$id'"
 
+    # 2. Convert the MP4 to WAV
+    WAV_PATH="${OUTPUT_PATH%.mp4}.wav"
+    if ! ffmpeg -i $OUTPUT_PATH -ac 1 $WAV_PATH; then
+        sqlite3 $DB_PATH "UPDATE tasks SET status='convert_failed' WHERE id='$id'"
+        continue
+    fi
+
+    # Update the task status to 'transcribing'
+    sqlite3 $DB_PATH "UPDATE tasks SET status='transcribing' WHERE id='$id'"
+
+    # 3. Transcribe the WAV (This is a placeholder, replace with your transcription command)
+    TXT_PATH="${WAV_PATH%.wav}.txt"
+    echo $WAV_PATH > $TXT_PATH
+
+    # Here you'll need to replace the above 'echo' command with your transcription method.
+    # After transcription, check if it was successful
+    if [ $? -eq 0 ]; then
+        sqlite3 $DB_PATH "UPDATE tasks SET status='completed' WHERE id='$id'"
+    
+        # If transcription was successful, delete the MP4 and WAV files
+        rm $OUTPUT_PATH
+        rm $WAV_PATH
+    
+    else
+        sqlite3 $DB_PATH "UPDATE tasks SET status='transcribe_failed' WHERE id='$id'"
+    fi
+done
