977723a61e930df0716d70f74557b39df1fd5b32
Raymon Mens <r.mens@me.com>
2023-11-01 17:17:52 +0100

    Install unzip

--- server.sh
+++ server.sh
@@ -1,7 +1,7 @@
 # Install needed packages
 apt update
 apt -qq -y install ffmpeg
-apt -qq -y install apache2 libapache2-mod-php php-sqlite3
+apt -qq -y install apache2 libapache2-mod-php php-sqlite3 unzip
 
 mkdir /opt/whisper
 wget -O /opt/whisper/source.zip https://github.com/ggerganov/whisper.cpp/archive/refs/tags/v1.4.0.zip
