3d8ad3a33a7dfc2f6ce66b362ca4dcd433fb5a87
Raymon Mens <r.mens@me.com>
2023-11-01 18:15:36 +0100

    Use master

--- server.sh
+++ server.sh
@@ -5,12 +5,12 @@ apt -qq -y install apache2 libapache2-mod-php php-sqlite3 unzip build-essential
 
 # Download Whisper
 mkdir /opt/whisper
-wget -O /opt/whisper/source.zip https://github.com/ggerganov/whisper.cpp/archive/refs/tags/v1.4.0.zip
+wget -O /opt/whisper/source.zip https://github.com/ggerganov/whisper.cpp/archive/refs/heads/master.zip
 cd /opt/whisper/
 unzip /opt/whisper/source.zip
 
 # Build Whisper
-cd /opt/whisper/whisper.cpp-1.4.0/
+cd /opt/whisper/whisper.cpp-master/
 make
 ./models/download-ggml-model.sh large
 ./main -m models/ggml-large.bin -f samples/jfk.wav
