a1204a842b1a5671c9468720c4f09dc0182e9e96
Raymon Mens <r.mens@me.com>
2023-11-01 17:17:29 +0100

    Update server.sh

--- server.sh
+++ server.sh
@@ -5,4 +5,5 @@ apt -qq -y install apache2 libapache2-mod-php php-sqlite3
 
 mkdir /opt/whisper
 wget -O /opt/whisper/source.zip https://github.com/ggerganov/whisper.cpp/archive/refs/tags/v1.4.0.zip
-unzip source.zip
+cd /opt/whisper/
+unzip /opt/whisper/source.zip
