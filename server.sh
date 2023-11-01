ce10842c94d6e71ad82e74cf9a8f44b9b3546dc6
Raymon Mens <r.mens@me.com>
2023-11-01 17:33:12 +0100

    Update server.sh

--- server.sh
+++ server.sh
@@ -3,11 +3,13 @@ apt update
 apt -qq -y install ffmpeg
 apt -qq -y install apache2 libapache2-mod-php php-sqlite3 unzip build-essential
 
+# Download Whisper
 mkdir /opt/whisper
 wget -O /opt/whisper/source.zip https://github.com/ggerganov/whisper.cpp/archive/refs/tags/v1.4.0.zip
 cd /opt/whisper/
 unzip /opt/whisper/source.zip
 
+# Build Whisper
 cd /opt/whisper/whisper.cpp-1.4.0/
 make
 ./models/download-ggml-model.sh large
